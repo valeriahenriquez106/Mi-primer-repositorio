8b615f3d1e28281aee69280a107533ef5281dfe0
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-05-20 16:10:21 -0600

    asignación python

---
+++ asignación.py
@@ -0,0 +1,16 @@
+x,y,z = "mandarina", "piña", "fresa"
+print(x)
+print(y)
+print(z)
+print("")
+x=y=z = "sandía"
+print(x)
+print(y)
+print(z)
+print("")
+fruta = ["mandarina", "piña", "fresa", "melón"]
+x,y,z, a = fruta
+print(x)
+print(y)
+print(z)
+print(a)
