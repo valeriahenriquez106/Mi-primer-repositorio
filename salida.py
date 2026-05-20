4ddd34883753dd647153bbe582cbf402796cd181
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-05-20 16:09:47 -0600

    salida python

---
+++ salida.py
@@ -0,0 +1,15 @@
+x,y,z = "Python", "is" ,"awesome"
+print(x,y,z)
+print("")
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+print("")
+x = 7
+y = 15
+print(x, y, z)
+print("")
+x = 5
+y = "John"
+print(x,y)
