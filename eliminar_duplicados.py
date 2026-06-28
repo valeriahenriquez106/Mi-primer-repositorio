0cd9e4ab35404c815c0c3c0b460d79d9f89a5ac1
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-28 14:39:57 -0600

    Create eliminar_duplicados.py

---
+++ eliminar_duplicados.py
@@ -0,0 +1,7 @@
+lista = [1, 2, 2, 3, 4, 4, 5, 5, 5]
+
+# Convertimos la lista a un "set" para borrar los duplicados, y luego lo volvemos a hacer lista
+lista_sin_duplicados = list(set(lista))
+
+# Mostramos el resultado
+print("Lista sin duplicados:", lista_sin_duplicados)
