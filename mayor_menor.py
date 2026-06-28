b8e20ffe9f2856238f2edb9678413ac0cac4a958
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-28 14:35:17 -0600

    Create mayor_menor.py

---
+++ mayor_menor.py
@@ -0,0 +1,7 @@
+numeros = [23, 45, 12, 67, 34, 89, 21]
+
+numero_mayor = max(numeros)
+numero_menor = min(numeros)
+
+print("El número mayor es:", numero_mayor)
+print("El número menor es:", numero_menor)
