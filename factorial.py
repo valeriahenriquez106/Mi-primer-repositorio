5fbdf560cecdd566b6a365d419207983b1edd0c1
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-28 09:11:04 -0600

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,10 @@
+numero = 5
+
+numero_original = numero
+resultado_factorial = 1
+
+while numero > 1:
+    resultado_factorial = resultado_factorial * numero
+    numero = numero - 1 
+
+print(resultado_factorial)
