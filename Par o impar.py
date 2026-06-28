1d386e07ad7f64e7f06854bc8b0c650508a650f2
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-28 08:59:28 -0600

    Create Par o impar.py

---
+++ Par o impar.py
@@ -0,0 +1,6 @@
+numero = int(input("Introduce el número entero: ")) 
+
+if numero % 2 == 0:
+    print(f"El número {numero} es PAR.")
+else:
+    print(f"El número {numero} es IMPAR.")                                                     
