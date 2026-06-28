b60c7898e73877cb2a0d781f715579e0aee85590
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-28 14:24:45 -0600

    Create lista_cuadrados.py

---
+++ lista_cuadrados.py
@@ -0,0 +1,5 @@
+# Usamos "list comprehension" para calcular los cuadrados del 1 al 10 en una sola línea
+cuadrados = [x**2 for x in range(1, 11)]
+
+# Mostramos la lista final en la pantalla
+print("Lista de cuadrados:", cuadrados)
