f4bb06b469db3d6eb4566e6fc54d59592b821bd5
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 20:04:23 -0600

    Create diccionario_paises.py

---
+++ diccionario_paises.py
@@ -0,0 +1,11 @@
+capitales = {
+    "El Salvador": "San Salvador",
+    "México": "Ciudad de México",
+    "España": "Madrid"
+}
+
+pais = "El Salvador"
+capital_especifica = capitales[pais]
+
+print("Diccionario completo:", capitales)
+print("La capital de", pais, "es:", capital_especifica)
