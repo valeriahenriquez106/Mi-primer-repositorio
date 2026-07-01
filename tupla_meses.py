d1db6bce0c2c248c97e82fa7bb24f32cf6c16bcb
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 19:52:45 -0600

    Create tupla_meses.py

---
+++ tupla_meses.py
@@ -0,0 +1,6 @@
+meses = ("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", 
+         "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre")
+
+mes_especifico = meses[4]
+
+print("El mes seleccionado es:", mes_especifico)
