13c69f4f1b024284b27d6be37c68269d9f5451ea
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 20:00:18 -0600

    Create tuplas_anidadas.py

---
+++ tuplas_anidadas.py
@@ -0,0 +1,9 @@
+datos = (("Juan", 25), ("María", 30), ("Pedro", 28))
+
+persona = datos[1]
+nombre_maria = datos[1][0]
+edad_maria = datos[1][1]
+
+print("Persona seleccionada:", persona)
+print("Nombre:", nombre_maria)
+print("Edad:", edad_maria)
