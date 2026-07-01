7c23144eec0e351931683c1a0b620e810ac96534
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 20:09:36 -0600

    Create notas_estudiante.py

---
+++ notas_estudiante.py
@@ -0,0 +1,8 @@
+notas = {"Ana": 85, "Luis": 92, "Carlos": 78}
+
+suma_notas = sum(notas.values())
+cantidad_estudiantes = len(notas)
+promedio = suma_notas / cantidad_estudiantes
+
+print("Notas de los estudiantes:", notas)
+print("El promedio del grupo es:", promedio)
