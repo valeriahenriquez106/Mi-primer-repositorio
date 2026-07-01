7a6553db59b752caf4a84a55735557e0d110ff24
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 19:55:27 -0600

    Create estadisticas_tupla.py

---
+++ estadisticas_tupla.py
@@ -0,0 +1,11 @@
+numeros = (10, 20, 30, 40, 50)
+
+suma_total = sum(numeros)
+
+valor_maximo = max(numeros)
+
+promedio = sum(numeros) / len(numeros)
+
+print("Suma total:", suma_total)
+print("Valor máximo:", valor_maximo)
+print("Promedio:", promedio)
