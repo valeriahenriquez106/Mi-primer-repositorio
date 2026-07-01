2ef019b9ba2b7c5f0373f639ebce9c7d8c8caa54
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 20:10:46 -0600

    Create traductor_basico.py

---
+++ traductor_basico.py
@@ -0,0 +1,7 @@
+traducciones = {"hola": "hello", "mundo": "world", "python": "python"}
+
+palabra_buscar = "hola"
+resultado = traducciones[palabra_buscar]
+
+print("Diccionario de traducción:", traducciones)
+print("La traducción de", palabra_buscar, "es:", resultado)
