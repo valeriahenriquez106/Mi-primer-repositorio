102c4df83da3df6b01a817f7896ecb768cfa1a1f
valeriahenriquez106 <valeriahenriquez106@gmail.com>
2026-06-30 20:19:15 -0600

    Create numero_positivo.py

---
+++ numero_positivo.py
@@ -0,0 +1,8 @@
+numero = float(input("Ingresa un número: "))
+
+if numero > 0:
+    print("El número es positivo.")
+elif numero < 0:
+    print("El número es negativo.")
+else:
+    print("El número es cero.")
